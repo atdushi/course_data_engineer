5050da8c7cfc1139429aefee415f435e422263a6
atdushi <atdushi@example.com>
2022-11-02 14:30:18 +0300

    bug fix

--- 2.2/FirstProject/src/main/scala/com/example/App.scala
+++ 2.2/FirstProject/src/main/scala/com/example/App.scala
@@ -3,36 +3,17 @@ package com.example
 import scala.io.StdIn.readLine
 
 object App {
-  var salaries: List[Int] = List(100, 150, 200, 80, 120, 75)
+  val salaries: List[Int] = List(100, 150, 200, 80, 120, 75)
 
   def main(args: Array[String]): Unit = {
-
     task_a
-
     task_b
-
     task_c
-
-    // d
-    val tuple = task_d(salaries, List(5, 5, 5, -5, 5, -5))
-    println(s"новая зарплата сотрудника: ${tuple._1.mkString(", ")}")
-    println(s"минимальная зарплата: ${tuple._2}")
-    println(s"максимальная зарплата: ${tuple._3}")
-
-    // e
-    salaries = task_e(salaries)
-    println(s"список сотрудников по уровню оклада от меньшего к большему: ${salaries.mkString(", ")}")
-
-    // f
-    salaries = task_f(salaries)
-    println(s"с новым сотрудник с зарплатой 130 тысяч: ${salaries.mkString(", ")}")
-
-    // g
-    println(s"специалисты уровня middle: ${task_g(salaries, 50, 110).mkString(", ")}")
-
-    // h
-    println(s"старая зарплата: ${salaries.mkString(", ")}")
-    println(s"проиндексированная зарплата: ${task_h(salaries).mkString(", ")}")
+    task_d
+    task_e
+    task_f
+    task_g
+    task_h
   }
 
   /*
@@ -63,7 +44,7 @@ object App {
     println("Компенсация питания:")
     val nutritional_compensation = readLine.toInt
 
-    println(s"ежемесячный оклад сотрудника после вычета налогов: ${Utils.compute_salary(salaryGross, bonus, nutritional_compensation)}")
+    println(s"ежемесячный оклад сотрудника после вычета налогов: ${Utils.computeSalary(salaryGross, bonus, nutritional_compensation)}")
   }
 
   /*
@@ -73,89 +54,87 @@ object App {
     аналогичные предыдущей программе, а также список со значениями окладов сотрудников отдела 100, 150, 200, 80, 120, 75.
    */
   def task_c() = {
-
     println("Введите годовой доход:")
     val salaryGross = readLine.toInt
-    println("Размер премии:")
-    val bonus = readLine.toFloat
-    println("Компенсация питания:")
-    val nutritional_compensation = readLine.toInt
 
-    val salaryNet = Utils.compute_salary(salaryGross, bonus, nutritional_compensation)
-    val deviation = Utils.compute_deviation(salaryNet, salaries)
+    val deviation = Utils.computeDeviation(salaryGross, salaries)
 
     println(s"отклонение(в процентах) от среднего значения оклада на уровень всего отдела: $deviation")
   }
 
   /*
-    d. Попробуйте рассчитать новую зарплату сотрудника, добавив(или отняв, если сотрудник плохо себя вел)
-    необходимую сумму с учетом результатов прошлого задания. Добавьте его зарплату в список и вычислите значение самой высокой зарплаты и самой низкой.
+    d. Попробуйте рассчитать новую зарплату сотрудника, добавив (или отняв, если сотрудник плохо себя вел)
+    необходимую сумму с учетом результатов прошлого задания.
+    Добавьте его зарплату в список и вычислите значение самой высокой зарплаты и самой низкой.
    */
-  def task_d(salaries: List[Int], bonus: List[Int]): (List[Int], Int, Int) = {
-    var result = List[Int]()
-    for ((elem, i) <- salaries.zipWithIndex) {
-      result = result :+ elem + bonus.apply(i)
-    }
-    val min = result.min
-    val max = result.max
+  def task_d() = {
+    val prevBonus = List(5, 5, 5, -5, 5, -5)
 
-    (result, min, max)
+    var result = List[Int]()
+
+    for ((elem, i) <- salaries.zipWithIndex) {
+      result = result :+ (elem + prevBonus.apply(i))
+    }
+
+    println(s"новая зарплата сотрудника: $result")
+    println(s"минимальная зарплата: ${result.min}")
+    println(s"максимальная зарплата: ${result.max}")
   }
 
   /*
     e. Также в вашу команду пришли два специалиста с окладами 350 и 90 тысяч рублей.
     Попробуйте отсортировать список сотрудников по уровню оклада от меньшего к большему.
    */
-  def task_e(salaries: List[Int]): List[Int] = {
+  def task_e() = {
     var result = salaries
     result = salaries :+ 350
     result = salaries :+ 90
-    result.sorted
+    result = result.sorted
+
+    println(s"список сотрудников по уровню оклада от меньшего к большему: ${result.mkString(", ")}")
   }
 
   /*
     f.  Кажется, вы взяли в вашу команду еще одного сотрудника и предложили ему оклад 130 тысяч.
     Вычислите самостоятельно номер сотрудника в списке так, чтобы сортировка не нарушилась и добавьте его на это место.
    */
-  def task_f(salaries: List[Int]): List[Int] = {
+  def task_f() = {
     var j = 0
     val salary = 130
 
     for ((elem, i) <- salaries.zipWithIndex) {
-      //      println(elem, j)
       if (elem < salary) {
         j = i
       }
     }
 
-    salaries.take(j + 1) ++ List(salary) ++ salaries.drop(j + 1)
+    val result = salaries.take(j + 1) ++ List(salary) ++ salaries.drop(j + 1)
+
+    println(s"с новым сотрудник с зарплатой 130 тысяч: ${result.mkString(", ")}")
   }
 
   /*
     g. Попробуйте вывести номера сотрудников из полученного списка, которые попадают под категорию middle.
     На входе программе подается «вилка» зарплаты специалистов уровня middle.
    */
-  def task_g(salaries: List[Int], min: Int, max: Int): List[Int] = {
-    var result = List[Int]()
-    for ((elem, i) <- salaries.zipWithIndex) {
-      //      println(elem, i)
-      if (elem < max & elem > min) {
-        result = result :+ i
-      }
-    }
-    result
+  def task_g() = {
+    println("Введите верхнюю границу для зарплаты middle:")
+    val min = readLine().toInt
+    println("Введите нижнюю границу для зарплаты middle:")
+    val max = readLine().toInt
+
+    println(s"специалисты уровня middle: ${Utils.computeFork(salaries, min, max).mkString(", ")}")
   }
 
   /*
     h. Однако наступил кризис и ваши сотрудники требуют повысить зарплату.
     Вам необходимо проиндексировать зарплату каждого сотрудника на уровень инфляции – 7%
    */
-  def task_h(salaries: List[Int]): List[Int] = {
-    var result = List[Int]()
-    salaries.foreach(c => {
-      result = result :+ (c + c * 0.07).round.toInt
-    })
-    result
-  }
+  def task_h() = {
+    println(s"старая зарплата: ${salaries.mkString(", ")}")
 
+    val inflation = 0.07f
+
+    println(s"проиндексированная зарплата: ${Utils.indexSalary(salaries, inflation).mkString(", ")}")
+  }
 }
--- 2.2/FirstProject/src/main/scala/com/example/Utils.scala
+++ 2.2/FirstProject/src/main/scala/com/example/Utils.scala
@@ -1,13 +1,33 @@
 package com.example
 
 object Utils {
-  def compute_salary(salaryGross: Int, bonus: Float, eatBonus: Int): Double = {
+  def computeSalary(salaryGross: Int, bonus: Float, eatBonus: Int): Double = {
     // 13 % налог
     (salaryGross + salaryGross * bonus + eatBonus) * 0.87 / 12
   }
 
-  def compute_deviation(salaryNet: Double, salaries: List[Int] ): Double = {
+  def computeDeviation(salaryGross: Int, salaries: List[Int]): Double = {
     val middleSalary = salaries.sum / salaries.length
-    100 - (100 * salaryNet / middleSalary)
+    100 - (100 * salaryGross / middleSalary)
+  }
+
+  def computeFork(salaries: List[Int], min: Int, max: Int): List[Int] = {
+    var result = List[Int]()
+
+    for ((elem, i) <- salaries.zipWithIndex) {
+      if (elem < max && elem > min) {
+        result = result :+ elem
+      }
+    }
+
+    result
+  }
+
+  def indexSalary(salaries: List[Int], inflation: Float): List[Double] = {
+    var result = List[Double]()
+    salaries.foreach(c => {
+      result = result :+ (c + c * inflation)
+    })
+    result
   }
 }
