3d99583e4327918dd0761eb381c213aed72cfaf4
atdushi <atdushi@example.com>
2022-11-02 13:14:17 +0300

    formula fix

--- 2.2/FirstProject/src/main/scala/com/example/Utils.scala
+++ 2.2/FirstProject/src/main/scala/com/example/Utils.scala
@@ -2,8 +2,8 @@ package com.example
 
 object Utils {
   def compute_salary(salaryGross: Int, bonus: Float, eatBonus: Int): Double = {
-    val month_total = salaryGross / 12 + (salaryGross * bonus) / 12 + eatBonus / 12
-    month_total * 0.87 // 13 % налог
+    // 13 % налог
+    (salaryGross + salaryGross * bonus + eatBonus) * 0.87 / 12
   }
 
   def compute_deviation(salaryNet: Double, salaries: List[Int] ): Double = {
